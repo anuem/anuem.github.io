992662e62cba6be115d5572c91b3a5635e979299
Think <treddys@me.com>
2017-03-07 19:30:15 +0100

    changed header and maps info

--- js/google_map.js
+++ js/google_map.js
@@ -1,6 +1,6 @@
 
 var google;
-
+var x;
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
@@ -18,7 +18,7 @@ function init() {
 
         // How you would like to style the map. 
         scrollwheel: false,
-        styles: [{"featureType": "all", "elementType": "labels.text.fill", "stylers": [{"saturation": 36}, {"color": "#26272c"}, {"lightness": 40}]}, {"featureType": "all", "elementType": "labels.text.stroke", "stylers": [{"visibility": "on"}, {"color": "#26272c"},{"lightness": 16}]}, {"featureType": "all", "elementType": "labels.icon", "stylers": [{"visibility": "off"}]}, {"featureType": "administrative", "elementType": "geometry.fill", "stylers": [{"color": "#26272c"}, {"lightness": 20}]},{"featureType": "administrative","elementType": "geometry.stroke","stylers":[{"color": "#26272c"},{"lightness":17},{"weight":1.2}]},{"featureType": "landscape","elementType": "geometry","stylers":[{"color": "#26272c"},{"lightness":20}]},{"featureType": "poi","elementType": "geometry","stylers":[{"color": "#26272c"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#26272c"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#26272c"}, {"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#26272c"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#26272c"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#26272c"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#26272c"},{"lightness":17}]}]
+        styles: [{"featureType": "all", "elementType": "labels.text.fill", "stylers": [{"saturation": 36}, {"color": "#26272c"}, {"lightness": 40}]}, {"featureType": "all", "elementType": "labels.text.stroke", "stylers": [{"visibility": "on"}, {"color": "#26272c"}, {"lightness": 16}]}, {"featureType": "all", "elementType": "labels.icon", "stylers": [{"visibility": "off"}]}, {"featureType": "administrative", "elementType": "geometry.fill", "stylers": [{"color": "#26272c"}, {"lightness": 20}]},{"featureType": "administrative", "elementType": "geometry.stroke", "stylers" : [{"color": "#26272c"}, {"lightness": 17}, {"weight": 1.2}]}, {"featureType": "landscape","elementType": "geometry","stylers":[{"color": "#26272c"},{"lightness": 20}]},{"featureType": "poi","elementType": "geometry","stylers": [{"color": "#26272c"}, {"lightness": 21}]}, {"featureType": "road.highway", "elementType": "geometry.fill","stylers": [{"color":"#26272c"},{"lightness": 17}]}, {"featureType": "road.highway","elementType": "geometry.stroke", "stylers":[{"color": "#26272c"}, {"lightness": 29}, {"weight": 0.2}]}, {"featureType": "road.arterial","elementType": "geometry","stylers":[{"color": "#26272c"}, {"lightness": 18}]},{"featureType": "road.local","elementType": "geometry","stylers":[{"color": "#26272c"},{"lightness": 16}]}, {"featureType": "transit","elementType": "geometry","stylers":[{"color": "#26272c"}, {"lightness": 19}]},{"featureType": "water","elementType": "geometry","stylers": [{"color": "#26272c"},{"lightness": 17}]}]
     };
 
     
@@ -29,12 +29,10 @@ function init() {
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
     var addresses = ['128, 5th Cross St, Pallavan Nagar, Thiruverkadu'];
-
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
+            var p = data.results[0].geometry.location;
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
                 position: latlng,
